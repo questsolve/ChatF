42ea5670cc746bba50a8914437b86ae1f2340019
kosmo05 <kosmo05@DESKTOP-26RO710>
2018-09-14 11:57:07 +0900

    commit 2018.09.14 friend dao, vo  수정

--- ChatF/src/main/java/com/chatf/friend/FriendVO.java
+++ ChatF/src/main/java/com/chatf/friend/FriendVO.java
@@ -2,34 +2,41 @@ package com.chatf.friend;
 
 public class FriendVO {
 
-	private int friend_no;
-	private String receiver_id;
-	private String sender_id;
-	private String friend_flag;
-	public int getFriend_no() {
-		return friend_no;
-	}
-	public void setFriend_no(int friend_no) {
-		this.friend_no = friend_no;
-	}
-	public String getReceiver_id() {
-		return receiver_id;
-	}
-	public void setReceiver_id(String receiver_id) {
-		this.receiver_id = receiver_id;
-	}
-	public String getSender_id() {
-		return sender_id;
-	}
-	public void setSender_id(String sender_id) {
-		this.sender_id = sender_id;
-	}
-	public String getFriend_flag() {
-		return friend_flag;
-	}
-	public void setFriend_flag(String friend_flag) {
-		this.friend_flag = friend_flag;
-	}
+	private int friendNo;
+	private String receiverId;
+	private String senderId;
+	private String friendFlag;
+	private String userId;
 	
-	
+	public int getFriendNo() {
+		return friendNo;
+	}
+	public void setFriendNo(int friendNo) {
+		this.friendNo = friendNo;
+	}
+	public String getReceiverId() {
+		return receiverId;
+	}
+	public void setReceiverId(String receiverId) {
+		this.receiverId = receiverId;
+	}
+	public String getSenderId() {
+		return senderId;
+	}
+	public void setSenderId(String senderId) {
+		this.senderId = senderId;
+	}
+	public String getFriendFlag() {
+		return friendFlag;
+	}
+	public void setFriendFlag(String friendFlag) {
+		this.friendFlag = friendFlag;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+		
 }
--- ChatF/src/main/java/com/chatf/friend/dao/FriendDao.java
+++ ChatF/src/main/java/com/chatf/friend/dao/FriendDao.java
@@ -1,8 +1,8 @@
 package com.chatf.friend.dao;
 
-import com.chatf.friend.friendVO;
+import com.chatf.friend.FriendVO;
 
-public interface friendDao {
+public interface FriendDao {
 
 //	insert into friend(friend_no,reciever_id,sender_id,friend_flag) values( friend_no_seq.nextval,?,?,?);
 //	select receiver_id,sender_id from friend where receiver_id=user_id or sender_id=user_id and friend_flag=’0’; ;
@@ -11,9 +11,9 @@ public interface friendDao {
 
 //4개 
 
-	public int addFriend(friendVO vo);
-	public void readFriend(friendVO vo);
-	public int updateFriend(friendVO vo);
+	public int addFriend(FriendVO vo);
+	public void readFriend(FriendVO vo);
+	public int updateFriend(FriendVO vo);
 	
 	
 }
